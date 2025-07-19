98fd72620065986e1370bceb08865764b2f58036
drichare <dorichare.hp@gmail.com>
2025-07-19 17:18:03 +0900

    Update content.js

--- news/content.js
+++ news/content.js
@@ -1,7 +1,8 @@
-    const newsData = [
-        { title: "ドリチャレ活動1周年", date: "2025/06/16", link: "item/2025/5/index.html", image: "img/1st.jpg" },
-        { title: "ドリチャレオンライン新商品追加お知らせ", date: "2025/05/09", link: "item/2025/4/index.html", image: "img/shop.png" },
-        { title: "不具合の修正について", date: "2025/05/09", link: "item/2025/3/index.html", image: "img/3.PNG" },
-        { title: "裏方募集のおしらせ", date: "2025", link: "item/2025/2/index.html", image: "img/ura.png" },
-        { title: "ドリチャレ公式HP開設", date: "2025", link: "item/2025/1/index.html", image: "img/rinku.png" }
-    ];
+const newsData = [
+    { title: "新メンバー加入のお知らせ", date: "2025/07/19", link: "item/2025/6/index.html", image: "item/2025/6/img/newmem.png" },
+    { title: "ドリチャレ活動1周年", date: "2025/06/16", link: "item/2025/5/index.html", image: "img/1st.jpg" },
+    { title: "ドリチャレオンライン新商品追加お知らせ", date: "2025/05/09", link: "item/2025/4/index.html", image: "img/shop.png" },
+    { title: "不具合の修正について", date: "2025/05/09", link: "item/2025/3/index.html", image: "img/3.PNG" },
+    { title: "裏方募集のおしらせ", date: "2025", link: "item/2025/2/index.html", image: "img/ura.png" },
+    { title: "ドリチャレ公式HP開設", date: "2025", link: "item/2025/1/index.html", image: "img/rinku.png" }
+];
