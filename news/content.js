9ee09ab449404596ee2d0d9e8bbc92c6ea3251eb
drichare <dorichare.hp@gmail.com>
2025-07-19 16:40:16 +0900

    Create content.js

---
+++ news/content.js
@@ -0,0 +1,7 @@
+    const newsData = [
+        { title: "ドリチャレ活動1周年", date: "2025/06/16", link: "item/2025/5/index.html", image: "img/1st.jpg" },
+        { title: "ドリチャレオンライン新商品追加お知らせ", date: "2025/05/09", link: "item/2025/4/index.html", image: "img/shop.png" },
+        { title: "不具合の修正について", date: "2025/05/09", link: "item/2025/3/index.html", image: "img/3.PNG" },
+        { title: "裏方募集のおしらせ", date: "2025", link: "item/2025/2/index.html", image: "img/ura.png" },
+        { title: "ドリチャレ公式HP開設", date: "2025", link: "item/2025/1/index.html", image: "img/rinku.png" }
+    ];
