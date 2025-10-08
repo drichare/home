e8f97cf96fa3be3d097d0cd6c9074872d699bd66
drichare <dorichare.hp@gmail.com>
2025-10-08 21:07:03 +0900

    Update news.js

--- js/news.js
+++ js/news.js
@@ -1,24 +1,21 @@
 // ニュースデータ
-    //最新のやつは上に
-    const newsData = [
-         { title: "10/7はりんくうの誕生日", date: "2025/10/07", link: "news/item/2025/12/index.html", image: "news/item/2025/12/img/birth.png" }
-    ];
+//最新のやつは上に
+const newsData = [
+    { title: "みゅーず加入のお知らせ", date: "2025/10/08", link: "news/item/2025/13/index.html", image: "news/item/2025/13/img/welmuse.png" }
+];
 
-    const newsList = document.getElementById("news-list");
-    newsData.forEach((news, index) => {
-        const li = document.createElement("li");
-        li.className = "news-item-wrapper";
-        li.innerHTML = `
-            <a href="${news.link}" class="news-item">
-                <img src="${news.image}" alt="${news.title}">
-                <div class="news-content">
-                    <span class="news-date">${news.date}</span>
-                    <h3>${news.title}</h3>
-                </div>
-            </a>
-        `;
-        newsList.appendChild(li);
-    });
-
-
-
+const newsList = document.getElementById("news-list");
+newsData.forEach((news, index) => {
+    const li = document.createElement("li");
+    li.className = "news-item-wrapper";
+    li.innerHTML = `
+        <a href="${news.link}" class="news-item">
+            <img src="${news.image}" alt="${news.title}">
+            <div class="news-content">
+                <span class="news-date">${news.date}</span>
+                <h3>${news.title}</h3>
+            </div>
+        </a>
+    `;
+    newsList.appendChild(li);
+});
